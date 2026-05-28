45cfbb921d05e9fd0a88fc10440bf4569bc039fd
Pkerasidis <p.kerasidis@icloud.com>
2026-05-28 11:36:37 +0300

    init mobile app

---
+++ app/src/main/java/com/pavloskerasidis/mobileapp_safecall/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.pavloskerasidis.mobileapp_safecall
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pavloskerasidis.mobileapp_safecall.ui.theme.MobileApp_SafeCallTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MobileApp_SafeCallTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobileApp_SafeCallTheme {
+        Greeting("Android")
+    }
+}
